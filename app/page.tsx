bc7f577e3ef5f60dc263737162202f76387a6cb6
Airz <apedrazatijaca@gmail.com>
2024-11-29 20:39:22 -0500

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import dynamic from "next/dynamic";
+
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
+
+export default App;
